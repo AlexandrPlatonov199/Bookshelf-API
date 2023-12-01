502d4af4cc88a52982d61c5e9caafbb6e96af53f
Alexander Platonov <sasa20sasha19910@yandex.ru>
2023-12-01 12:16:59 +0300

    add admin-panel

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from sqladmin import Admin
 
+from src.admin_panel.views import BookAdmin, CategoryAdmin, AuthorAdmin
 from src.books.authors.routers import router as router_author
 from src.books.categorys.routers import router as router_category
 from src.books.routers import router as router_book
+from src.db import async_engine
 
 app = FastAPI(title="Bookshelf")
 
 app.include_router(router_author)
 app.include_router(router_book)
 app.include_router(router_category)
+
+admin = Admin(app, async_engine)
+
+admin.add_view(BookAdmin)
+admin.add_view(CategoryAdmin)
+admin.add_view(AuthorAdmin)
