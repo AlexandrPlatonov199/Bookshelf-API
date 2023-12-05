20e52c231b3c83dfb1a99437edbfee65a0f5f3a9
Alexander Platonov <sasa20sasha19910@yandex.ru>
2023-12-05 14:48:21 +0300

    add pytest settings

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from dotenv import load_dotenv, find_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -5,6 +7,7 @@ load_dotenv(find_dotenv(".env"))
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
 
     DB_USER: str
     DB_PASS: str
@@ -15,6 +18,16 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_HOST: str
+    TEST_DB_NAME: str
+    TEST_DB_PORT: int
+
+    @property
+    def TEST_DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
     SECRET_KEY: str
     ALGORITHM: str
--- src/db.py
+++ src/db.py
@@ -1,10 +1,15 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
-from typing_extensions import AsyncGenerator
 
 from src.config import settings
 
-
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
 class Base(DeclarativeBase):
     __abstract__ = True
 
@@ -15,10 +20,5 @@ class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True)
 
 
-async_engine = create_async_engine(settings.DATABASE_URL)
+async_engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 async_sessionmaker = async_sessionmaker(async_engine, expire_on_commit=False)
-
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_sessionmaker() as session:
-        yield session
